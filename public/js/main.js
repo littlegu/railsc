08694be4edbefd5f40992cab48a3eb3c7d7848af
littlegu <kuznecova@ek.apress.ru>
2013-02-20 18:08:27 +0600

    railsc: mail

--- public/js/main.js
+++ public/js/main.js
@@ -9,4 +9,6 @@ $doc.ready(function() {
     $(this).closest(".vacancy").toggleClass('show-');
   });
 
+  $('.js-job-mail').replaceWith('<a href="ma' + 'ilt' + 'o:job@rai' + 'lsc.' + 'ru">jo' + 'b@r' + 'ail' + 'sc.ru</a>');
+
 });
