842433f6c7317161b76f24ab07a63f6f2a19f919
littlegu <kuznecova@ek.apress.ru>
2013-02-27 08:28:54 +0600

    railsc: dancing

--- public/js/main.js
+++ public/js/main.js
@@ -1,19 +1,56 @@
 var
   $doc = $(document),
-  $win = $(window);
+  $win = $(window),
+  kitty = {
+    body: $('.kitty'),
 
-function kitty() {
-  $('.kitty').removeClass('gohome-')
+    poseNum: 0,
+    
+    dance: function() {
+      kitty._move = setInterval(function() { 
+        kitty.poseNum ++;
+        kitty.changePose(kitty.poseNum);
+        if (kitty.poseNum == 8) {
+          kitty.poseNum = 0;
+        }
+      }, 200);  
+    },
+    
+    changePose: function(n) {
+      kitty.body.attr('class', 'kitty step-' + n);
+    },
+    
+    stop: function() {
+      clearInterval(kitty._move);
+    },
+    
+    _move: null
+  };
+  
+function kittyActivity() {
+  $('.kitty').removeClass('meow')
     .animate({
-      right: "-40",
-      bottom: "-70"
+      right: "-10",
+      bottom: "-40"
     }, 2000)
-    .on('mouseover', function() {
-      var $this = $(this);
-      $this.addClass('gohome-');
+    .on('mouseenter', function() {
+      kitty.stop();
+      kitty.dance();      
+    })
+    .on('mouseleave', function() {
+      kitty.stop();
+    })
+    .on('click', function() {
+      var 
+        $this = $(this);
+      
+      kitty.stop();
+      $this.attr('class', 'kitty meow');
+            
       setTimeout(function() {
-        $this.removeClass('gohome-');
+        $this.removeClass('meow');
       }, 600);
+      
       setTimeout(function() {
         $this.animate({
           bottom: "-" +  + $this.height()
@@ -28,10 +65,12 @@ $doc.ready(function() {
   $('.js-v-head').click(function() {
     $(this).closest(".vacancy").toggleClass('show-');
   });
-
+  
+  /* Замена спанов мылом */
   $('.js-job-mail').replaceWith('<a href="ma' + 'ilt' + 'o:job@rai' + 'lsc.' + 'ru">jo' + 'b@r' + 'ail' + 'sc.ru</a>');
 
-  kitty();
-  setInterval(function() { kitty(); }, 20000 );
+  /* Активность котика */
+  kittyActivity();
+  setInterval(function() { kittyActivity(); }, 20000 );
 
 });
